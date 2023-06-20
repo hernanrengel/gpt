a6f0bafdc3cd10e50d2903bf1d36bc6c239cdb5e
hrengelc <hrengelc@gmail.com>
2023-06-20 00:05:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# chatgpt
+
+1. Open the terminal and navigate to the project folder.
+2. Run the command `flutter packages get` to install the required packages.
+3. Run `flutter run` to build and run the debug app on your emulator/phone
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:chatgpt/theme.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 2000)).then((value) {
+      Navigator.of(context).pushReplacement(PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            const ChatScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.bounceOut;
+
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBg500Color,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/logo.png",
+                height: 65,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
